db22bdb7dbf94ed73649a8b70634f09f4879b928
RajSanjel <rajsanjel201@gmail.com>
2022-07-22 15:15:12 +0545

    Basic Setup Done

---
+++ src/components/Users.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import User from "./subComponent/User";
+function Users() {
+  return (
+    <div>
+      Users
+      <User />
+    </div>
+  );
+}
+
+export default Users;
